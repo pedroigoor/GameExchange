cfd2ec8b12ae45b09d2f343f31eece77a31a0c8e
Pedro almeida <pedro.igor10@live.com>
2026-03-21 17:16:11 -0300

    Created Entity Listings

---
+++ src/Backend/GameExchange.Domain/Entities/Listing.cs
@@ -0,0 +1,19 @@
+﻿using GameExchange.Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameExchange.Domain.Entities
+{
+    public class Listing : EntityBase
+    {
+        public long SellerId { get;  set; }
+        public long GameId { get;  set; }
+        public string Title { get;  set; } = string.Empty;
+        public string Description { get;  set; } = string.Empty;
+        public decimal Price { get;  set; }
+        public ListingStatus Status { get;  set; }
+
+
+    }
+}
---
+++ src/Backend/GameExchange.Domain/Enum/ListingStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace GameExchange.Domain.Enum
+{
+    public enum ListingStatus
+    {
+        Draft = 0,
+        Active = 1,
+        Sold = 2,
+        Cancelled = 3
+    }
+}
